e5b7d5e3a01c952912adc8ef9448ef491e6c7a04
Alfonso Saavedra Son <sonlink.dourden@gmail.com>
2012-04-03 13:19:38 +0200

    r3: Ahora se usa Curses para mostrar la interfaz del juego

--- ahorcapy.py
+++ ahorcapy.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  ahorcapy.py
+#  Ahorcapy r3
 #  
-#  Copyright 2011 Alfonso Saavedra "Son Link" <sonlink.dourden@gmail.com>
+#  Copyright 2012 Alfonso Saavedra "Son Link" <sonlink.dourden@gmail.com>
 #  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -23,15 +23,34 @@
 #  
 
 # Versión del juego del ahorcado.
-# (c) 2011 Alfonso Saavedra "Son Link"
+# (c) 2011-2012 Alfonso Saavedra "Son Link"
+# http://sonlinkblog.blogspot.com
 
-from os import system
-from re import search, UNICODE
+from re import search
 from random import randint
+import curses
+
+# Iniciamos Curses
+stdscr = curses.initscr()
+curses.start_color()
+# Definimos los pares de color que usaremos
+curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
+curses.noecho()
+curses.cbreak()
+
+# Configuramos e iniciamos la ventana del juego
+begin_x = 1 ; begin_y = 1
+height = 20 ; width = 40
+win = curses.newwin(height, width, begin_y, begin_x)
 
 class Ahorcapy():
 	
 	def __init__(self):
+		"""
+		Iniciamos el juego
+		"""
 		
 		self.positions = []
 		self.letters = []
@@ -46,33 +65,33 @@ class Ahorcapy():
 		
 		self.word2 = ''
 		
-		i = 0
+		i = 1
 		while i <= len(self.word):
 			self.word2 += '_'
 			i += 1
 		
-		self.interface = [
-		'\tAhorcapy. (c) 2011 Alfonso Saavedra "Son Link"\n',
-		'\t_______',
-		'\t/     |',
-		'\t|',
-		'\t|',
-		'\t|',
-		'\t|\n',
-		'\tPalabra:%s\n' % self.word2,
-		'\tLetras introducidas hasta el momento:\n']
+		stdscr.addstr(1, 4, 'Ahorcapy. (c) 2011 Alfonso Saavedra "Son Link"' , curses.color_pair(3) )
+		stdscr.addstr(3, 4, '_______')
+		stdscr.addstr(4, 4, '/     |')
+		stdscr.addstr(5, 4, '|')
+		stdscr.addstr(6, 4, '|')
+		stdscr.addstr(7, 4, '|')
+		stdscr.addstr(8, 4, '|')
+		stdscr.addstr(10, 4, 'Palabra: %s' % self.word2)
+		stdscr.refresh()
 		
-		self.redraw()
-		
-	def checkLetter(self, key):
-				
+		self.checkLetter()
+	
+	def checkLetter(self):
+		"""
+		Comprueba si la letra esta en la palabra.
+		"""
+		key = chr(stdscr.getch())
 		if search('[A-Za-z]$', key):
-			
 			if not key in self.letters:
-			
 				self.letters.append(key)
 				self.letters.sort()
-				
+					
 				if key in self.word:
 					i = -1
 					try:
@@ -83,9 +102,9 @@ class Ahorcapy():
 					except ValueError:
 						pass
 				else:
-					self.beep()
+					curses.beep()
 					self.errors += 1
-						
+							
 				i = 0
 				word2 = ''
 				while i < len(self.word):
@@ -96,81 +115,74 @@ class Ahorcapy():
 					i += 1
 					
 				self.word2 = word2
-			
-			
+							
 			else:
-				self.beep()
+				curses.beep()
 				self.errors += 1
 			
-		self.redraw()
+			self.redraw()
+		
+		elif key == 27:
+			# Si se pulsa la tecla Esc se cierra el juego
+			self.salir()
 
 	def redraw(self):
-		system("clear")
+		"""
+		Repinta la ventana
+		"""
 		letters = ''
 				
 		if self.word2 != self.word:
 			
 			if self.errors == 1:
-				self.interface[3] = '\t|     O'
+				stdscr.addstr(5, 4, '|     O')
 			
 			if self.errors == 2:
-				self.interface[4] = '\t|     |'
+				stdscr.addstr(6, 4, '|     |')
 				
 			if self.errors == 3:
-				self.interface[4] = '\t|    /|'
+				stdscr.addstr(6, 4, '|    /|')
 				
 			if self.errors == 4:
-				self.interface[4] = '\t|    /|\\'
+				stdscr.addstr(6, 4, '|    /|\\')
 				
 			if self.errors == 5:
-				self.interface[5] = '\t|    /'
+				stdscr.addstr(7, 4, '|    /')
 				
 			if self.errors == 6:
-				self.interface[5] = '\t|    / \\'
-				self.interface[7] = '\tPalabra: %s\n' % self.word2
-				for line in self.interface:
-					print ('\x1b[1;31m'+line)
+				stdscr.addstr(7, 4, '|    / \\')
+				stdscr.addstr(10, 4, 'Palabra: %s' % self.word2)
 					
-				print ('\t¡HAS FALLADO!\x1b[0m')
+				stdscr.addstr(12, 4, '¡Has fallado!', curses.color_pair(1) )
 				self.retry()
 				
-			self.interface[7] = '\tPalabra: %s\n' % self.word2
+			stdscr.addstr(10, 4, 'Palabra: %s' % self.word2)
 			
 			for k in self.letters:
 				letters += ' ' + k
 				
-			self.interface[8] = '\tLetras introducidas hasta el momento:%s\n' % letters
-			
-			for line in self.interface:
-				print ('\x1b[1m'+line+'\x1b[0m')
-			
-			key = raw_input('\tEscriba una letra: ')
-			
-			self.checkLetter(key)
+			stdscr.addstr(14, 4, 'Letras introducidas hasta el momento:%s' % letters)
+			stdscr.refresh()
+			self.checkLetter()
 		
 		else:
-			self.interface[7] = '\tPalabra: %s\n' % self.word2			
-			for line in self.interface:
-				print ('\x1b[1;32m'+line)
-			
-			print ('\t¡HAS GANADO!\x1b[0m')
+			stdscr.addstr(10, 4, 'Palabra: %s' % self.word2)
+			stdscr.addstr(12, 4, '¡Has ganado!', curses.color_pair(2) )
 			
 			self.retry()
-			
-	def beep(self):
-		f=open('/dev/tty','w')
-		f.write(chr(7))
-		f.close()
-		
 	
 	def retry(self):
+		"""
+		Al terminar la partida nos preguntara si queremos volver a jugar
+		"""
+		stdscr.addstr(16, 4, 'Desea jugar otra vez [S/n]')
+		key = chr(stdscr.getch())
 		
-		q = raw_input('\tComenzar otra partida [s/N]: ')
-		
-		if q == 's' or q == 'S':
+		if key == 's' or key == 'S':
 			self.positions = []
 			self.letters = []
 			self.errors = 0
+			stdscr.clear()
 			
 			f = open('words.txt', 'r')
 			words = f.readlines()
@@ -181,29 +193,31 @@ class Ahorcapy():
 			
 			self.word2 = ''
 			
-			i = 0
+			i = 1
 			while i <= len(self.word):
 				self.word2 += '_'
 				i += 1
 			
-			self.interface = [
-			'\tAhorcapy. (c) 2011 Alfonso Saavedra "Son Link"\n',
-			'\t_______',
-			'\t/     |',
-			'\t|',
-			'\t|',
-			'\t|',
-			'\t|\n',
-			'\tPalabra:%s\n' % self.word2,
-			'\tLetras introducidas hasta el momento:\n']
-			
+			stdscr.addstr(1, 4, 'Ahorcapy. (c) 2011 Alfonso Saavedra "Son Link"' , curses.color_pair(3) )
+			stdscr.addstr(3, 4, '_______')
+			stdscr.addstr(4, 4, '/     |')
+			stdscr.addstr(5, 4, '|')
+			stdscr.addstr(6, 4, '|')
+			stdscr.addstr(7, 4, '|')
+			stdscr.addstr(8, 4, '|')
+			stdscr.addstr(10, 4, 'Palabra: %s' % self.word2)
+			stdscr.refresh()			
 			self.redraw()
 			
-		elif q == 'n' or q == 'N' or not q:
-			exit()
+		elif key == 'n' or key == 'N':
+			self.salir()
 			
-		else:
-			self.redraw()
+	def salir(self):
+		"""
+		Salimos del programa
+		"""
+		curses.endwin()
+		exit()
 			
 if __name__ == '__main__':
 	Ahorcapy()
